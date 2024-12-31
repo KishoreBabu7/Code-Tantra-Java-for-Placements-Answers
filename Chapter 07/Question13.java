fdcab5848cbbefdde37f3949e1c0ab059f49a32e
TULUGU KISHORE BABU <157289017+KishoreBabu7@users.noreply.github.com>
2024-12-31 14:30:06 +0530

    Update Question13.java

--- Chapter 07/Question13.java
+++ Chapter 07/Question13.java
@@ -41,7 +41,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 public class BigDecimalUsage {
 	public static void main(String[] args) {
-		MathContext mathContext = new MathContext(3);
+		MathContext mathContext = new MathContext(5);
 		BigDecimal x = new BigDecimal("3.14159265358", mathContext);
 		System.out.print(x);
 	}
